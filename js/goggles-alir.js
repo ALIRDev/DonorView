a6d4d29a55942d4b3dbad0e82fe291ada89e7d4b
Andrea <andrea.zago@medas-solutions.it>
2018-02-23 16:42:51 +0100

    Sample request

--- js/goggles-alir.js
+++ js/goggles-alir.js
@@ -15,3 +15,73 @@ function testcall() {
     });
 
 }
+
+function testcall2() {
+
+    var userId =  "4";
+    var donationDate = "2016-05-18T16:00:00Z";
+    var expirationDate = "2016-05-18T16:00:00Z";
+    var userSteamId = "76561197971046908";
+    var donationAmount = "5";
+    var adminNotes = "ff";
+
+
+    var requestData = "?userId=" + userId + "&donationDate=" + donationDate + "&expirationDate=" + expirationDate + "&userSteamId=" + userSteamId + "&donationAmount=" + donationAmount + "&adminNotes=" + adminNotes;
+
+    $.ajax({
+        url: " http://192.168.30.77:8000/donations" + requestData,
+        type: 'POST',
+        timeout: 5000,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ("Authorization", "Basic " + btoa(requestUser + ":" + requestPass));
+        }
+    }).done(function (data) {
+        console.log(data);
+    });
+
+}
+
+function testcall3(id) {
+
+    var requestData = "?id=" + id;
+
+    $.ajax({
+        url: " http://192.168.30.77:8000/donations" + requestData,
+        type: 'DELETE',
+        timeout: 5000,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ("Authorization", "Basic " + btoa(requestUser + ":" + requestPass));
+        }
+    }).done(function (data) {
+        console.log(data);
+    });
+
+}
+
+function testcall4(id) {
+
+    var userId =  "77";
+    var donationDate = "2016-05-18T16:00:00Z";
+    var expirationDate = "2016-05-18T16:00:00Z";
+    var userSteamId = "76561197971046908";
+    var donationAmount = "5";
+    var adminNotes = "ff";
+
+    console.log(id);
+
+    var requestData = "?id=" + id +"&userId=" + userId + "&donationDate=" + donationDate + "&expirationDate=" + expirationDate + "&userSteamId=" + userSteamId + "&donationAmount=" + donationAmount + "&adminNotes=" + adminNotes;
+
+
+    $.ajax({
+        url: " http://192.168.30.77:8000/donations" + requestData,
+        type: 'PUT',
+        timeout: 5000,
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader ("Authorization", "Basic " + btoa(requestUser + ":" + requestPass));
+        }
+    }).done(function (data) {
+        console.log(data);
+    });
+
+}
+

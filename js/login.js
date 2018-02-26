644a4655e690553be66f69458fee64ebbc9bcc8e
Andrea <andrea.zago@medas-solutions.it>
2018-02-26 17:49:15 +0100

    login code

--- js/login.js
+++ js/login.js
@@ -1,29 +1,92 @@
-function loadLogins() {
-    console.log("dsa");
-    console.log(Cookies.get('MyALIR-1967usr894'));
+function checkCookies() {
+
+    var loggedInInfo = Cookies.get('myalir-uulog');
+
+    if (loggedInInfo) {
+
+        $.ajax({
+            url: "http://192.168.30.77:8000/checksec",
+            type: 'GET',
+            dataType: "json",
+            timeout: 5000,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Authorization", "Basic " + loggedInInfo);
+            },
+            statusCode: {
+                401: function (xhr) {
+                    if (window.console) {
+                        $('#alertErrorLogin').attr('hidden', true);
+                        $('#notAllowedData').removeAttr('hidden');
+                    }
+                }
+            }
+        }).done(function () {
+            window.location = "http://localhost:63342/ALIRDonorGoggles/index.html?_ijt=iiq15vbsmf83t9ce140agcf4j7";
+        });
+
+    }
+
 }
-loadLogins();
 
-function loginHand(username, password) {
+checkCookies();
 
-    Cookies.set('MyALIR-1967usr894', username, {expires: 7});
-    Cookies.set('MyALIR-1967pas642', password, {expires: 7});
-}
+function checkUserAuth() {
 
-function checkUserAuth(username, password) {
 
-    var authLogin = "Basic " + btoa(username + ":" + password);
+    var username = $('#userName').val();
+    var password = $('#userPass').val();
 
-    $.ajax({
-        url: " http://192.168.30.77:8000/checksec",
-        type: 'GET',
-        dataType: "json",
-        timeout: 5000,
-        beforeSend: function (xhr) {
-            xhr.setRequestHeader ("Authorization", authLogin);
-        }
-    }).done(function (data) {
-        console.log(data);
-    });
+    var r_text = [];
+    r_text[0] = "Noooooooo!";
+    r_text[1] = "Ma!";
+    r_text[2] = "Non capisco!";
+    r_text[3] = "Ma che fai!";
+    r_text[4] = "Perchè?";
+    r_text[5] = "Accidenti!";
+    r_text[6] = "Ritenta XD";
+
+    var i = Math.floor(7 * Math.random());
+
+    if (!username && !password) {
+        $('#alertErrorLogin').removeAttr('hidden');
+        $('#errorStrong').html(r_text[i]);
+        $('#errorSpan').html("Username e password sono obbligatori!");
+        console.log("Username error");
+    } else if (!username) {
+        $('#alertErrorLogin').removeAttr('hidden');
+        $('#errorStrong').html(r_text[i]);
+        $('#errorSpan').html("Hai dimenticato l'username!");
+        console.log("Password error")
+    } else if (!password) {
+        $('#alertErrorLogin').removeAttr('hidden');
+        $('#errorStrong').html(r_text[i]);
+        $('#errorSpan').html("Non vedo la password!");
+        console.log("Password error")
+    } else {
+
+        var authLogin = btoa(username + ":" + password);
+
+        $.ajax({
+            url: "http://192.168.30.77:8000/checksec",
+            type: 'GET',
+            dataType: "json",
+            timeout: 5000,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("Authorization", "Basic " + authLogin);
+            },
+            statusCode: {
+                401: function (xhr) {
+                    if (window.console) {
+                        $('#alertErrorLogin').attr('hidden', true);
+                        $('#notAllowedData').removeAttr('hidden');
+                    }
+                }
+            }
+        }).done(function (data) {
+            Cookies.set('myalir-uulog', authLogin, {expires: 20});
+            //window.location = "http://localhost:63342/ALIRDonorGoggles/index.html?_ijt=iiq15vbsmf83t9ce140agcf4j7";
+        });
+
+    }
 
 }
